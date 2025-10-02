c9fb7530f21f46854b1cf2401ca46ea1755e348d
Eucatastrophe <108638101+makrcat@users.noreply.github.com>
2025-10-01 20:53:58 -0700

    eee

--- script.js
+++ script.js
@@ -1,49 +1,52 @@
+
+
 function start() {
+    startGame();
     console.log("started")
     fadeOutStart();
-    startGame();
 }
+
+
 
 function fadeOutStart() {
     document.getElementById("gps").classList.add("fade");
 }
 
 function startGame() {
-
     const woof = document.getElementById("woof");
     const arf = document.getElementById("arf");
 
-
-
+    // horrible way of doing this but it's ok lmao
     const spriteBig = document.getElementById("big");
-    const bigAnimal = new Animal(
-        spriteBig,
-        "big_idle1.png",
-        "big_idle2.png",
-        "big_sing.png",
-        "a",
-        woof
-    );
-
-
     const spriteSmall = document.getElementById("small");
-    const smallAnimal = new Animal(
-        spriteSmall,
-        "small_idle1.png",
-        "small_idle2.png",
-        "small_sing.png",
-        "s",
-        arf
-    );
 
-    
+    const spritePuff = document.getElementById("puff");
+    const spritePlant = document.getElementById("plant");
+    const spriteOg = document.getElementById("og");
+    const spritePink = document.getElementById("pink");
+    const spriteGreen = document.getElementById("green");
+    const spriteFire = document.getElementById("fire");
+    const spriteBlue = document.getElementById("blue");
+
+
+    const thingys = [
+        new thingy(spriteBig, "big_idle1.png", "big_idle2.png", "big_sing.png", "a", woof),
+        new thingy(spriteSmall, "small_idle1.png", "small_idle2.png", "small_sing.png", "s", arf),
+        new thingy(spritePuff, "puff1.png", "puff2.png", "puff3.png", "q", arf),
+        new thingy(spritePlant, "plant1.png", "plant2.png", "plant3.png", "w", arf),
+        new thingy(spriteOg, "og1.png", "og2.png", "og3.png", "s", arf),
+        new thingy(spritePink, "pink1.png", "pink2.png", "pink3.png", "e", arf),
+        new thingy(spriteGreen, "green1.png", "green2.png", "green3.png", "d", arf),
+        new thingy(spriteFire, "fire1.png", "fire2.png", "fire2.png", "z", arf),
+        new thingy(spriteBlue, "blue1.png", "blue2.png", "blue3.png", "x", arf),
+    ];
+
     setInterval(() => {
-        bigAnimal.updateIdle();
-        smallAnimal.updateIdle();
+        thingys.forEach(thingy => thingy.updateIdle());
     }, 500);
 }
 
-class Animal {
+class thingy {
     constructor(element, idle1, idle2, singImg, charKey, audioEl) {
         this.element = element;
         this.idle1 = idle1;
@@ -89,12 +92,22 @@ class Animal {
 
     stopSing() {
         if (this.isSinging) {
-
             this.isSinging = false;
             this.element.src = this.currentIdle;
-
             this.element.style.filter = "none";
         }
     }
-
 }
+
+
+let loaderRotation = 0;
+
+window.addEventListener("keydown", (e) => {
+    if (e.key.toLowerCase() === "c") {
+        loaderRotation -= 5;
+        const loader = document.getElementById("loader");
+        if (loader) {
+            loader.style.transform = `rotate(${loaderRotation}deg)`;
+        }
+    }
+});
